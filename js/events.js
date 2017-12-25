96a80d1e5059c5623a9fadabe871f0c85109c28e
Sonik S <gryazzz@gmail.com>
2017-12-25 20:52:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt() {
 }
 
 function submitIt() {
-  $('form input:first-child').on('submit', function() {
+  $('form').on('submit', function() {
     alert('Your form is going to be submitted now.');
   });
 }
